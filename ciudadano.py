4f79f6b44e745c7b368534203c56bfb898d8d954
lfcb87 <144630928+lfcb87@users.noreply.github.com>
2023-11-16 22:59:22 -0500

    Add files via upload

---
+++ ciudadano.py
@@ -0,0 +1,41 @@
+class ciudadano():
+    def__init__(self,nombre,edad,cedula)
+       self_.nombre=nombre
+       self._edad=edad
+       self._cedula=cedula
+     
+    
+    #---------setters--------
+
+def setNombre(self,nombre:str):
+    self._nombre=nombre
+def setEdad(self,edad:int):
+    self._codigo=codigo
+def setCedula(self,cedula:str):
+    self._cedula=cedula
+
+    #----------getters--------
+
+def getNombre(self,nombre:str):
+    return self._nombre
+def getEdad(self,edad:str):
+    return self.codigo
+def getCedula(self,cedula:str):
+    return self.cedula
+
+def edad():
+    n=getEdad()
+    if n>18:
+    print(f'mayor de edad')
+    else print(f'menor de edad')
+
+def main()
+    nombre=input('Nombre: ')
+    edad=input('Edad: ')
+    cedula=input('Cedula: ')
+ print(f'Nombre: {usuario.getNombre()}\n'\
+              f'edad: {usuario.getEdad()}\n'\
+                f'Cedula: {usuario.geCedula()}\n'\)
+
+if __name__==__main__
+    main()
